53615b29bb47b9d89f19f9235b91e59fdee05f5d
Patrick Dumoulin <1312444@cegeplimoilou.ca>
2024-04-09 20:55:45 -0400

    Added React et API

---
+++ react_user_interface/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ImageUploadForm from './Components/ImageUploadForm'
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Upload d'image</h1>
+      <ImageUploadForm />
+    </div>
+  );
+}
+
+export default App;
